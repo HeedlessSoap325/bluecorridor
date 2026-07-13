ca0058c373dad91a32b4f8fbcb63afd55f6a759b
M. Fritz <mathisjulian123@gmail.com>
2026-07-13 22:15:04 +0200

    fix: naming

--- export.go
+++ export.go
@@ -11,7 +11,7 @@ func exportCMD(args []string) {
 	help := fs.Bool("help", false, "Print this message")
 
 	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s list [options]\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s export [options]\n\n", os.Args[0])
 		fmt.Fprintln(os.Stderr, "Options:")
 		fs.PrintDefaults()
 	}
