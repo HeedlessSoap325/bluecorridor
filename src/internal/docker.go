0c15addd746a55d48defed5eb3c6e1539531a722
M. Fritz <mathisjulian123@gmail.com>
2026-07-22 09:26:24 +0200

    feat: add warning if DOCKER_HOST isn't set

--- src/internal/docker.go
+++ src/internal/docker.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/moby/moby/client"
 )
@@ -17,6 +18,16 @@ func InitializeDockerClient() error {
 	// Deinitialize previous docker API client if it exists
 	if dockerClient != nil {
 		DeinitializeDockerClient()
+	}
+
+	if value, _ := os.LookupEnv("DOCKER_HOST"); value == "" {
+		fmt.Print("\033[38;5;214m")
+		fmt.Println("[WARNING] Your environment does not appear to provide the DOCKER_HOST environment variable")
+		fmt.Println("[WARNING] The DOCKER_HOST is used to connect to the right docker socket, without it you may see different resources than you expect or none at all")
+		fmt.Printf("[WARNING] The default DOCKER_HOST value that will be used is '%s'\n", client.DefaultDockerHost)
+		fmt.Println("[WARNING] To avoid this warning, please export DOCKER_HOST in your environment")
+		fmt.Println("[WARNING] If you are unsure on which docker host is the right one, you can run 'docker context ls' to see your current and all other available hosts")
+		fmt.Print("\033[0m")
 	}
 
 	var err error
