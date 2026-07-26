329e5547ac4f908323a5bf4f70a9115dd49f1476
M. Fritz <mathisjulian123@gmail.com>
2026-07-26 21:08:45 +0200

    fix: use internal printing helpers in main.go

--- src/main.go
+++ src/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/heedlesssoap325/bluecorridor/commands"
+	"github.com/heedlesssoap325/bluecorridor/internal/printing"
 )
 
 func main() {
 	if err := commands.HandleCommand(); err != nil {
-		fmt.Fprintf(os.Stderr, "\033[31m%s\033[0m\n", err)
+		printing.PrintWithColoredForeground(os.Stderr, printing.ERROR, "%s\n", err)
 		os.Exit(1)
 	}
 }
