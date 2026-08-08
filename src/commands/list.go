f4f4cade8deffe37160be7e0a7b9415f5bfd1510
M. Fritz <mathisjulian123@gmail.com>
2026-08-08 22:10:01 +0200

    feat: use tables for list command

--- src/commands/list.go
+++ src/commands/list.go
@@ -8,6 +8,9 @@ import (
 
 	"github.com/heedlesssoap325/bluecorridor/internal/console"
 	"github.com/heedlesssoap325/bluecorridor/internal/docker"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/api/types/image"
+	"github.com/moby/moby/api/types/volume"
 )
 
 func handleList(args []string) error {
@@ -27,111 +30,228 @@ func handleList(args []string) error {
 		fs.Usage()
 	}
 
-	var totalExportSize int64 = 0
+	if err := listContainers(); err != nil {
+		return err
+	}
 
+	if err := listVolumes(false); err != nil { // TODO: give the user the option to display the size as well
+		return err
+	}
+
+	if err := listImages(false); err != nil { // TODO: give the user the option to display the pullability as well
+		return err
+	}
+
+	if err := listNetworks(); err != nil {
+		return err
+	}
+
+	if !*quiet {
+		fmt.Println()
+		exportSize, err := estimateExportSize()
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(os.Stdout, "Estimated size of uncompressed export file: %s\n", exportSize)
+		fmt.Println("The above estimate may be noticeably below or above the actual size due to, among other factors, compression.")
+	}
+
+	return nil
+}
+
+func listContainers() error {
 	containers, err := docker.ContainerList(nil)
 	if err != nil {
 		return err
 	}
 
-	if !*quiet {
-		fmt.Println("Containers:")
+	titles := []string{"NAMES", "ID", "STATE", "STATUS"}
+
+	rows := make([][]string, 0, len(containers))
+	for _, container := range containers {
+		rows = append(rows, []string{
+			containerNames(container),
+			container.ID[:12],
+			string(container.State),
+			container.Status,
+		})
 	}
 
-	if len(containers) <= 0 {
-		fmt.Println("    No Containers found")
-	} else {
-		for _, container := range containers {
-			containerName, _ := strings.CutPrefix(container.Names[0], "/")
-			fmt.Fprintf(os.Stdout, "    %s\n", containerName)
-		}
+	fmt.Printf("\n### CONTAINERS ###\n\n")
+	console.PrintTable(titles, rows, 3)
+
+	return nil
+}
+
+func containerNames(c container.Summary) string {
+	names := make([]string, len(c.Names))
+
+	for i, name := range c.Names {
+		names[i] = strings.TrimPrefix(name, "/")
 	}
 
+	return strings.Join(names, ", ")
+}
+
+func listVolumes(includeSize bool) error {
 	volumes, err := docker.VolumeList(nil)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println()
-	if !*quiet {
-		fmt.Println("Volumes:")
+	titles := []string{"NAME", "SIZE", "SCOPE"}
+
+	rows := make([][]string, 0, len(volumes))
+	for _, volume := range volumes {
+
+		rows = append(rows, []string{
+			volume.Name,
+			volumeSize(volume, includeSize),
+			volume.Scope,
+		})
 	}
 
-	if len(volumes) <= 0 {
-		fmt.Println("    No volumes found")
-	} else {
-		for _, volume := range volumes {
-			volumeSize, err := docker.VolumeSize(volume.Name)
-			if err != nil {
-				return err
-			}
+	fmt.Printf("\n### VOLUMES ###\n\n")
+	console.PrintTable(titles, rows, 3)
 
-			fmt.Fprintf(os.Stdout, "    %s (%s)\n", volume.Name, console.FormatBytes(volumeSize))
-			totalExportSize += volumeSize
-		}
+	return nil
+
+}
+
+func volumeSize(v volume.Volume, includeSize bool) string {
+	if !includeSize {
+		return "unknown"
 	}
 
+	volumeSize, err := docker.VolumeSize(v.Name)
+	if err != nil {
+		return "unknown"
+	}
+	return console.FormatBytes(volumeSize)
+}
+
+func listImages(includePullable bool) error {
 	images, err := docker.ImageList(nil)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println()
-	if !*quiet {
-		fmt.Println("Images:")
+	titles := []string{"IMAGE", "ID", "DISK USAGE", "PULLABLE", "IN USE"}
+
+	rows := make([][]string, 0, len(images))
+	for _, image := range images {
+		rows = append(rows, []string{
+			imageName(image),
+			image.ID,
+			console.FormatBytes(image.Size),
+			imagePullable(image, includePullable),
+			imageInUse(image),
+		})
 	}
 
-	if len(images) <= 0 {
-		fmt.Println("    No images found")
-	} else {
-		for _, image := range images {
-			imageName := "<none>:<none>"
-			if len(image.RepoTags) > 0 {
-				imageName = image.RepoTags[0]
-			}
+	fmt.Printf("\n### IMAGES ###\n\n")
+	console.PrintTable(titles, rows, 3)
 
-			method, _ := docker.DetermineTransferMethod(image.RepoTags, image.RepoDigests)
-			switch method {
-			case docker.MethodPull:
-				fmt.Fprintf(os.Stdout, "    %s (pullable)\n", imageName)
+	return nil
+}
 
-			case docker.MethodSaveLoad:
-				imageSize, err := docker.ImageSize(image.ID)
-				if err != nil {
-					return err
-				}
-
-				fmt.Fprintf(os.Stdout, "    %s (>=%s)\n", imageName, console.FormatBytes(imageSize))
-				totalExportSize += imageSize
-			}
-		}
+func imageName(i image.Summary) string {
+	if len(i.RepoTags) == 0 {
+		return "<none>:<none>"
 	}
 
+	return i.RepoTags[0]
+}
+
+func imagePullable(i image.Summary, includePullable bool) string {
+	if !includePullable {
+		return "unknown"
+	}
+
+	switch method, _ := docker.DetermineTransferMethod(i.RepoTags, i.RepoDigests); method {
+	case docker.MethodPull:
+		return "Yes"
+	case docker.MethodSaveLoad:
+		return "No"
+	default:
+		return "unknown"
+	}
+}
+
+func imageInUse(i image.Summary) string {
+	switch {
+	case i.Containers == 0:
+		return "No"
+	case i.Containers > 0:
+		return "Yes"
+	default:
+		return "unknown" // if image.Containers is -1, the field hasn't been set / calculated yet
+	}
+}
+
+func listNetworks() error {
 	networks, err := docker.NetworkList(nil)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println()
-	if !*quiet {
-		fmt.Println("Networks:")
+	titles := []string{"NAME", "ID", "DRIVER", "SCOPE"}
+
+	rows := make([][]string, 0, len(networks))
+	for _, network := range networks {
+		if docker.NetworkNameReserved(network.Name) {
+			continue
+		}
+
+		rows = append(rows, []string{
+			network.Name,
+			network.ID,
+			network.Driver,
+			network.Scope,
+		})
 	}
 
-	if len(networks)-3 <= 0 { // Don't show Built-In networks, and there are three of them
-		fmt.Println("    No custom networks found")
-	} else {
-		for _, network := range networks {
-			if !docker.NetworkNameReserved(network.Name) {
-				fmt.Fprintf(os.Stdout, "    %s\n", network.Name)
-			}
+	fmt.Printf("\n### NETWORKS ###\n\n")
+	console.PrintTable(titles, rows, 3)
+
+	return nil
+}
+
+func estimateExportSize() (string, error) {
+	console.PrintWithColoredForeground(os.Stdout, console.INFO, "Estimating export size...")
+	var totalExportSize int64 = 0
+
+	volumes, err := docker.VolumeList(nil)
+	if err != nil {
+		return "", err
+	}
+
+	for _, volume := range volumes {
+		volumeSize, err := docker.VolumeSize(volume.Name)
+		if err != nil {
+			return "", err
+		}
+
+		totalExportSize += volumeSize
+	}
+
+	images, err := docker.ImageList(nil)
+	if err != nil {
+		return "", err
+	}
+
+	for _, image := range images {
+		switch method, _ := docker.DetermineTransferMethod(image.RepoTags, image.RepoDigests); method {
+		case docker.MethodPull:
+
+		case docker.MethodSaveLoad:
+			totalExportSize += image.Size
+		default:
+			return "", fmt.Errorf("Pullability of image was unknown")
 		}
 	}
 
-	if !*quiet {
-		fmt.Println()
-		fmt.Fprintf(os.Stdout, "Estimated size of uncompressed export file: %s\n", console.FormatBytes(totalExportSize))
-		fmt.Println("The above estimate may be noticeably below or above the actual size due to, among other factors, compression.")
-	}
+	console.ClearNLinesAndPositionCursorAtStart(1)
 
-	return nil
+	return console.FormatBytes(totalExportSize), nil
 }
