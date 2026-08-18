1f632535931f7d9f7625d4bbf4ff947d273c97b0
M. Fritz <mathisjulian123@gmail.com>
2026-08-18 21:19:26 +0200

    refactor: convert from spaces to tabs

--- src/internal/compression/tar.go
+++ src/internal/compression/tar.go
@@ -11,62 +11,62 @@ import (
 )
 
 func Tar(srcDir, tarFile string) error {
-    out, err := os.Create(tarFile)
-    if err != nil {
-        return err
-    }
-    defer out.Close()
+	out, err := os.Create(tarFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 
-    gz := gzip.NewWriter(out)
+	gz := gzip.NewWriter(out)
 	defer gz.Close()
 
 	tw := tar.NewWriter(gz)
-    defer tw.Close()
+	defer tw.Close()
 
-    return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
+	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-        header, err := tar.FileInfoHeader(info, "")
-        if err != nil {
-            return err
-        }
+		header, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
 
-        relPath, err := filepath.Rel(srcDir, path)
-        if err != nil {
-            return err
-        }
+		relPath, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			return err
+		}
 
-        header.Name = filepath.ToSlash(relPath)
+		header.Name = filepath.ToSlash(relPath)
 
-        if err := tw.WriteHeader(header); err != nil {
-            return err
-        }
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
 
-        if info.IsDir() {
-            return nil
-        }
+		if info.IsDir() {
+			return nil
+		}
 
-        file, err := os.Open(path)
-        if err != nil {
-            return err
-        }
-        defer file.Close()
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
 
-        _, err = io.Copy(tw, file)
-        return err
-    })
+		_, err = io.Copy(tw, file)
+		return err
+	})
 }
 
 func Untar(tarFile, dest string) error {
-    in, err := os.Open(tarFile)
-    if err != nil {
-        return err
-    }
-    defer in.Close()
+	in, err := os.Open(tarFile)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
 
-    gz, err := gzip.NewReader(in)
+	gz, err := gzip.NewReader(in)
 	if err != nil {
 		return err
 	}
@@ -74,54 +74,54 @@ func Untar(tarFile, dest string) error {
 
 	tr := tar.NewReader(gz)
 
-    for {
-        hdr, err := tr.Next()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            return err
-        }
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
 
-        name := filepath.FromSlash(hdr.Name)
-        target := filepath.Join(dest, name)
+		name := filepath.FromSlash(hdr.Name)
+		target := filepath.Join(dest, name)
 
-        // zip-slip quard
-        if !strings.HasPrefix(target, filepath.Clean(dest)+string(os.PathSeparator)) {
-            if hdr.Name == "." {
-                continue
-            }
-            return fmt.Errorf("illegal file path in tar: %s", hdr.Name)
-        }
+		// zip-slip quard
+		if !strings.HasPrefix(target, filepath.Clean(dest)+string(os.PathSeparator)) {
+			if hdr.Name == "." {
+				continue
+			}
+			return fmt.Errorf("illegal file path in tar: %s", hdr.Name)
+		}
 
-        switch hdr.Typeflag {
-        case tar.TypeDir:
-            if hdr.Name == "." {
-                continue
-            }
-            if err := os.MkdirAll(target, os.FileMode(hdr.Mode)); err != nil {
-                return err
-            }
+		switch hdr.Typeflag {
+		case tar.TypeDir:
+			if hdr.Name == "." {
+				continue
+			}
+			if err := os.MkdirAll(target, os.FileMode(hdr.Mode)); err != nil {
+				return err
+			}
 
-        case tar.TypeReg:
-            if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
-                return err
-            }
+		case tar.TypeReg:
+			if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
+				return err
+			}
 
-            f, err := os.OpenFile(target,
-                os.O_CREATE|os.O_RDWR|os.O_TRUNC,
-                os.FileMode(hdr.Mode))
-            if err != nil {
-                return err
-            }
+			f, err := os.OpenFile(target,
+				os.O_CREATE|os.O_RDWR|os.O_TRUNC,
+				os.FileMode(hdr.Mode))
+			if err != nil {
+				return err
+			}
 
-            if _, err := io.Copy(f, tr); err != nil {
-                f.Close()
-                return err
-            }
-            f.Close()
-        }
-    }
+			if _, err := io.Copy(f, tr); err != nil {
+				f.Close()
+				return err
+			}
+			f.Close()
+		}
+	}
 
-    return nil
+	return nil
 }
